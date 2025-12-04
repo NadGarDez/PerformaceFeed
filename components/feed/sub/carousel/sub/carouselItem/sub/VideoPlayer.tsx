c464bd5e7dfc5ceb436b0d005567379dfb35944e
Nad GarDez <iranad@hotmail.com>
2025-12-04 12:23:00 -0400

    feat: added expo video configuration

--- components/feed/sub/carousel/sub/carouselItem/sub/VideoPlayer.tsx
+++ components/feed/sub/carousel/sub/carouselItem/sub/VideoPlayer.tsx
@@ -1,7 +1,6 @@
-import { useEvent } from 'expo';
 import { useVideoPlayer, VideoView } from 'expo-video';
 import { JSX, memo } from 'react';
-import { Button, StyleSheet, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 
 interface props {
@@ -18,23 +17,9 @@ export const VideoPlayer = memo(
       player.play();
     });
 
-    const { isPlaying } = useEvent(player, 'playingChange', { isPlaying: player.playing });
-
     return (
       <View style={styles.contentContainer}>
         <VideoView style={styles.video} player={player} />
-        <View style={styles.controlsContainer}>
-          <Button
-            title={isPlaying ? 'Pause' : 'Play'}
-            onPress={() => {
-              if (isPlaying) {
-                player.pause();
-              } else {
-                player.play();
-              }
-            }}
-          />
-        </View>
       </View>
     );
   }
@@ -43,17 +28,13 @@ export const VideoPlayer = memo(
 const styles = StyleSheet.create({
   contentContainer: {
     flex: 1,
-    padding: 10,
     alignItems: 'center',
     justifyContent: 'center',
-    paddingHorizontal: 50,
   },
   video: {
     width: '100%',
     height: 300,
     borderRadius: 10
   },
-  controlsContainer: {
-    padding: 10,
-  },
+ 
 });
